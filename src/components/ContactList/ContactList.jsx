55ed84cf81fa39716e6b48f1ce6b608b6fe98e61
Vikto0004 <vikto0004@gmail.com>
2024-06-27 13:28:11 +0200

    finish

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,1 +1,13 @@
-// import css from "ContactList.module.css";
+import css from "./ContactList.module.css";
+
+import Contact from "../Contact/Contact";
+
+export default function ContactList({ contacts, onDelete }) {
+  return (
+    <ul className={css.list}>
+      {contacts.map((data) => (
+        <Contact key={data.id} data={data} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
+}
